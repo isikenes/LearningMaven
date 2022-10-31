32f7fb861661e91e03b7fc9cba669d0bbf6390af
Enes Işık <30756694+isikenes@users.noreply.github.com>
2022-10-31 16:01:38 +0300

    uploaded files

---
+++ src/main/java/com/learningmaven/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.learningmaven.helloworld;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
